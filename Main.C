af976065cb6e7722ecadced44bbc104d27091191
EliasMenn <eliasmennella10@gmail.com>
2023-08-12 00:08:54 -0300

    Added Several Functions and the start of the program

--- Main.C
+++ Main.C
@@ -1,13 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 void VectEnCero(int Vect[], int X);
+int VerDato(int Dato, int LimInf, int LimSup);
 
 int main()
 {
  int vect[10];
  int flag = 0;
  int cantidadPin;
+ VectEnCero(vect, 10);
+
+ srand(time(NULL));
+ printf("Please confirm if you wish to add character repetition (0) or not (1)");
+ scanf("%d", flag);
+ flag=VerDato(flag,0,1);
+ if (flag==1)
+ {
+    printf("Please enter how many characters you wish to use for the pin code (Integer between 1 and 10)");
+    scanf("%d", &cantidadPin);
+    cantidadPin=VerDato(cantidadPin, 1, 10);
+ }
+
 
 
 }
@@ -18,4 +33,17 @@ void VectEnCero(int Vect[], int X)
     {
         Vect[i]=0;
     }
+}
+int VerDato(int Dato, int LimInf, int LimSup)
+{
+    do
+    {
+        if(Dato<LimInf || Dato>LimSup)
+        {
+            printf("Please enter a valid number between %d and %d", LimInf, LimSup);
+            scanf("%d", &Dato);
+        }
+    }
+    while(Dato<LimInf || Dato>LimSup);
+    return Dato;
 }
