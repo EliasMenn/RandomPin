3a97ff7eb28f5b311c3c1fa488420c9a2cdab1c8
EliasMenn <eliasmennella10@gmail.com>
2023-08-11 23:37:27 -0300

    Added Variables and some functions

--- Main.C
+++ Main.C
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <time.h>
+
+void VectEnCero(int Vect[], int X);
 
 int main()
 {
+ int vect[10];
+ int flag = 0;
+ int cantidadPin;
+
+
+}
+void VectEnCero(int Vect[], int X)
+{
+    int i = 0;
+    for(i=0;i<X;i++)
+    {
+        Vect[i]=0;
+    }
 }
